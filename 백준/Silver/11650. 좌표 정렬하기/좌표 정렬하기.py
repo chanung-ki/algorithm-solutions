79782344e46f27c108331220da3553bdce99d128
chanung-ki <117616574+chanung-ki@users.noreply.github.com>
2025-02-07 15:08:35 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 280 ms, Memory: 54216 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,8 @@
+import sys
+n = int(sys.stdin.readline())
+coordinates = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
+
+coordinates.sort(key=lambda x: (x[0], x[1]))
+
+for coordinate in coordinates:
+    sys.stdout.write(f'{coordinate[0]} {coordinate[1]}' + '\n')
